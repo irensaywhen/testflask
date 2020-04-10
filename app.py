396c84d90de39ce489fcdc2373b2f6fbeac4cc27
Lindsey Smith <lindsey.smith@streamlineinnovations.com>
2020-04-10 14:08:59 -0500

    Chapter 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/index')
+def index():
+    return 'Hello, Flask!'
+
+@app.route('/hello/<name>')
+@app.route('/hello/')
+def hello(name=None):
+    if name is None:
+        abort(404)
+
+    return 'Hello, %s' % name
 
 
 if __name__ == '__main__':
